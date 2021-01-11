8e2d0bdcc4d830890f05ba36d360ba95e42a8a4e
mabeijing <58149278@qq.com>
2021-01-11 23:31:04 +0800

    配置文件

---
+++ dbUtils/_config.py
@@ -0,0 +1,72 @@
+# redis_config = {"host": "localhost",
+#                 "password": "",
+#                 "port": 6379,
+#                 "db": 0,
+#                 "decode_responses": True,  # 转化字符串，False是字节
+#                 "encoding": "utf-8",
+#                 "encoding_errors": "strict"}
+
+# oms_db_ini = {
+#     'host': 'localhost',
+#     'port': 3306,
+#     'database': 'oms_db',
+#     'user': 'root',
+#     'password': 'root@123',
+#     'charset': 'utf8mb4',
+#     'use_unicode': True
+# }
+# ams_db_ini = {
+#     'host': 'localhost',
+#     'port': 3306,
+#     'database': 'ams_db',
+#     'user': 'root',
+#     'password': 'root@123',
+#     'charset': 'utf8mb4',
+#     'use_unicode': True
+# }
+# mms_db_ini = {
+#     'host': 'localhost',
+#     'port': 3306,
+#     'database': 'mms_db',
+#     'user': 'root',
+#     'password': 'root@123',
+#     'charset': 'utf8mb4',
+#     'use_unicode': True
+# }
+
+
+redis_config = {"host": "192.168.88.232",
+                "password": "Root@123",
+                "port": 6379,
+                "db": 0,
+                "decode_responses": True,  # 转化字符串，False是字节
+                "encoding": "utf-8",
+                "encoding_errors": "strict"}
+
+oms_db_ini = {
+    'host': '172.20.20.29',
+    'port': 3306,
+    'database': 'oms_db',
+    'user': 'test',
+    'password': 'TeSt163@pybgRey',
+    'charset': 'utf8mb4',
+    'use_unicode': True
+}
+ams_db_ini = {
+    'host': '172.20.20.29',
+    'port': 3306,
+    'database': 'ams_db',
+    'user': 'test',
+    'password': 'TeSt163@pybgRey',
+    'charset': 'utf8mb4',
+    'use_unicode': True
+}
+mms_db_ini = {
+    'host': '172.20.20.29',
+    'port': 3306,
+    'database': 'mms_db',
+    'user': 'test',
+    'password': 'TeSt163@pybgRey',
+    'charset': 'utf8mb4',
+    'use_unicode': True
+}
